ab5263e1a3adcd217f8faafbfac6f71d13695cfb
Hossein Salari <hosein.salari81@gmail.com>
2024-08-13 01:34:45 +0330

    feat: create module for save category in db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { CategoryModule } from "./category/category.module";
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://127.0.0.1:27017/teamgit-weblog")],
+  imports: [
+    MongooseModule.forRoot("mongodb://127.0.0.1:27017/teamgit-weblog"),
+    CategoryModule,
+  ],
   controllers: [],
   providers: [],
 })
