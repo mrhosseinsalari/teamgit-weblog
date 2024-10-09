f6d12507668ce0269518b165c6f1edbcb74f044d
Hossein Salari <hosein.salari81@gmail.com>
2024-10-09 17:40:09 +0330

    feat: find all blogs and singleBlog by id

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,23 +1,28 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
+  Query,
   UseInterceptors,
 } from "@nestjs/common";
-import { ApiConsumes, ApiTags } from "@nestjs/swagger";
+import { ApiConsumes, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { BlogService } from "./blog.service";
 import { CreateBlogDto } from "./dto/create.dto";
 import { MulterFile } from "src/common/types/public";
 import { CheckRequiredUploadedFile } from "src/common/decorator/upload-file.decorator";
 import { UploadFile } from "src/common/interceptor/upload-file.interceptor";
 import { MIME_TYPE } from "src/common/enum/meme-type.enum";
+import { Blog } from "./schema/blog.schema";
 
 @ApiTags("Blog")
 @Controller("/blog")
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
+
   @Post()
   @ApiConsumes("multipart/form-data")
   @UseInterceptors(UploadFile("image"))
@@ -32,5 +37,22 @@ export class BlogController {
       statusCode: HttpStatus.CREATED,
       message: "created blog successfully",
     };
+  }
+
+  @Get()
+  @ApiQuery({ name: "search", type: "string", required: false })
+  @ApiQuery({ name: "category", type: "string", required: false })
+  async findAll(
+    @Query("search") search: string,
+    @Query("category") category: string
+  ): Promise<{ blogs: Blog[] }> {
+    const blogs = await this.blogService.findAll({ search, category });
+    return { blogs };
+  }
+
+  @Get(":id")
+  @ApiParam({ name: "id", type: "string" })
+  async findOne(@Param("id") id: string): Promise<Blog> {
+    return await this.blogService.findOne(id);
   }
 }
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -4,6 +4,7 @@ import { Model, Types } from "mongoose";
 import { CategoryService } from "src/category/category.service";
 import { InjectModel } from "@nestjs/mongoose";
 import { CreateBlogDto } from "./dto/create.dto";
+import { ISearch } from "./interfaces/blog.interface";
 
 @Injectable()
 export class BlogService {
@@ -23,5 +24,59 @@ export class BlogService {
       category: new Types.ObjectId(Number(category._id)),
     });
     return newBlog;
+  }
+
+  async findAll(query: ISearch) {
+    let match = {};
+    if (query.search) {
+      match["$or"] = [
+        { title: { $regex: query.search, $options: "gi" } },
+        { content: { $regex: query.search, $options: "gi" } },
+      ];
+    }
+    if (query.category) {
+      match["category"] = { $regex: query.category, $options: "gi" };
+    }
+    const blogs = await this.blogModel.aggregate([
+      {
+        $lookup: {
+          from: "categories",
+          localField: "category",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+      { $unwind: "$category" },
+      {
+        $addFields: {
+          category: "$category.name",
+        },
+      },
+      {
+        $match: match,
+      },
+    ]);
+    return blogs;
+  }
+
+  async findOne(id: string) {
+    const blog = await this.blogModel.aggregate([
+      {
+        $lookup: {
+          from: "categories",
+          localField: "category",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+      { $unwind: "$category" },
+      {
+        $addFields: {
+          category: "$category.name",
+        },
+      },
+    ]);
+
+    return blog?.[0];
   }
 }
